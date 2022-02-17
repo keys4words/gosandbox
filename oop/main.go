eafeac515cafbed25d090677a7d799339e3fc07f
keys4words <keys4words@gmail.com>
2022-02-17 17:12:13 +0300

    add sort to oop

--- oop/main.go
+++ oop/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Employee struct {
-	name string
+	name     string
 	position string
-	salary int
+	salary   int
 	currency string
 }
 
@@ -15,12 +18,33 @@ func (e Employee) DisplayInfo() {
 	fmt.Printf("salary: %v %s\n", e.salary, e.currency)
 }
 
+type agentSlice []Employee
+
+func (as agentSlice) Len() int           { return len(as) }
+func (as agentSlice) Less(i, j int) bool { return as[i].salary < as[j].salary }
+func (as agentSlice) Swap(i, j int)      { as[i], as[j] = as[j], as[i] }
+
 func main() {
 	emp := Employee{
-		name: "James Position",
-		position:"Agent 007",
-		salary: 5500,
+		name:     "James Bond",
+		position: "Agent 007",
+		salary:   5500,
 		currency: "pounds",
 	}
 	emp.DisplayInfo()
+	emp2 := Employee{
+		name:     "Jason Bourn",
+		position: "Agent ANB",
+		salary:   6500,
+		currency: "dollar",
+	}
+	emp3 := Employee{
+		name:     "Bashirov & Petrov",
+		position: "Agent KGB",
+		salary:   10,
+		currency: "wood",
+	}
+	agents := agentSlice{emp, emp2, emp3}
+	sort.Sort(agents)
+	fmt.Println(agents)
 }
