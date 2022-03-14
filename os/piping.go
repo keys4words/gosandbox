85ca9799eab94729974af530432160bdc9b210d9
keys4words <keys4words@gmail.com>
2022-03-14 12:04:30 +0300

    finished piping in os

--- os/piping.go
+++ os/piping.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -26,9 +28,46 @@ func Piping() {
 	io.Copy(os.Stdout, &b2)
 
 	fmt.Println(b2)
-
 }
+func InputPiping() {
+	cmd := exec.Command("cat")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, "write from goroutine")
+	}()
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", out)
+}
+func OutputPiping() {
+	cmd := exec.Command("echo", "piping in output pipe")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
 
+	data, err := ioutil.ReadAll(stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("%s\n", string(data))
+}
 func main() {
-	Piping()
+	// Piping()
+	// InputPiping()
+	OutputPiping()
 }
