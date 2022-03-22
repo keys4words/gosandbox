3dab2df82495a3c32425560bcbb7eec2290b15de
keys4words <keys4words@gmail.com>
2022-03-22 09:01:06 +0300

    finished files

--- files/unbuffered.go
+++ files/unbuffered.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -15,19 +16,20 @@ func check(e error) {
 func read(filepath string) {
 	f, err := os.Open(filepath)
 	check(err)
+
 	b1 := make([]byte, 9)
 	n1, err := f.Read(b1)
 	check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 
-	// b2 := make([]byte, 9)
-	// n2, err := f.Read(b2)
-	// check(err)
-	// fmt.Printf("%v\n", string(b2[:n2]))
-
-	o3, err := f.Seek(6, 2)
+	b2 := make([]byte, 9)
+	n2, err := f.Read(b2)
 	check(err)
-	b3 := make([]byte, 2)
+	fmt.Printf("%d bytes: %s\n", n2, string(b2[:n2]))
+
+	o3, err := f.Seek(5, 1)
+	check(err)
+	b3 := make([]byte, 9)
 	n3, err := io.ReadAtLeast(f, b3, 2)
 	check(err)
 	fmt.Printf("%d bytes @ %d %s\n", n3, o3, string(b3))
@@ -39,12 +41,26 @@ func write(filepath string) {
 	check(err)
 
 	defer f.Close()
+
 	o2 := []byte{115, 111, 109, 101, 10}
 	n2, err := f.Write(o2)
 	check(err)
 	fmt.Printf("wrote %d bytes\n", n2)
+
+	o3, err := f.WriteString("new line\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", o3)
+
+	_ = f.Sync()
+
+	w := bufio.NewWriter(f)
+	o4, err := w.WriteString("buffered string\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", o4)
+	w.Flush()
 }
 
 func main() {
-	read("test.txt")
+	// read("test.txt")
+	write("output.txt")
 }
