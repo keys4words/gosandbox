a206a9e9984520a65d0bf2555e988cb2373e1f08
keys4words <keys4words@gmail.com>
2022-04-09 20:29:16 +0300

    finished postgres

--- dbPractice/postgres.go
+++ dbPractice/postgres.go
@@ -14,7 +14,7 @@ import (
 
 func PostgresSimpleQuery() {
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), "postgres://admin:admin@localhost:5432/mydb")
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:example@localhost:5432/postgres")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
@@ -22,39 +22,35 @@ func PostgresSimpleQuery() {
 	defer conn.Close(context.Background())
 
 	var name string
-	var address string
-	var salary int
-	var age int
+	var position string
 	var id int64
-	err = conn.QueryRow(context.Background(), "select * from company where id=$1", 2).Scan(&id, &name, &age, &address, &salary) //сырой интерфейс записывает значения в списки полей, возможности смаппить каждое поле нет
+	err = conn.QueryRow(context.Background(), "select * from users where id=$1", 2).Scan(&id, &name, &position)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(name, age, address, salary)
+	fmt.Println(name, position)
 }
 
 func PostgresScanToObject() {
 	ctx := context.Background()
-	db, _ := pgxpool.Connect(ctx, "postgres://admin:admin@localhost:5432/mydb")
+	db, _ := pgxpool.Connect(ctx, "postgres://postgres:example@localhost:5432/postgres")
 
 	type User struct {
-		Name    string
-		Age     int
-		Address string
-		Salary  int
-		Id      int
+		Name string
+		Rank string
+		Id   int
 	}
 
 	defer db.Close()
 
 	var users []User
-	pgxscan.Select(ctx, db, &users, "select * from company") //библиотечка хелпер, которая маппит поля за вас, Select маппит списки, когда в запросе больше одного элемента
+	pgxscan.Select(ctx, db, &users, "select * from users") //lib to map to slice
 	fmt.Println(users)
 
 	var user User
-	pgxscan.Get(ctx, db, &user, "select * from company where id=$1", 2) //Get берет одно значение и маппит в конкретный обьект
+	pgxscan.Get(ctx, db, &user, "select * from users where id=$1", 2) //get & map 1 job
 	fmt.Println(user)
 }
 
@@ -93,5 +89,7 @@ func PostgresAddCar(userId int, colour string, brand string, licensePlate string
 
 func main() {
 	// PostgresSimpleQuery()
-	PostgresScanToObject()
+	// PostgresScanToObject()
+	// PostgresJoinQuery()
+	PostgresAddCar(9, "orange", "Lexus", "OEM")
 }
