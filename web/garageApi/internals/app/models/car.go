3840e97127f42b501de542c18b4e95b50ff4488e
keys4words <keys4words@gmail.com>
2022-04-18 18:28:03 +0300

    mv garageApi to go-restful

--- web/garageApi/internals/app/models/car.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Car struct {
-	Id           int64 `json:"id"`
-	Colour       string `json:"colour"`
-	Brand        string `json:"brand"`
-	LicensePlate string `json:"license_plate"`
-	Owner        User  `json:"owner"`
-}
