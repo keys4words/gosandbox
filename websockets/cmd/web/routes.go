31f0b18f473fba8caad6b36dd3f3757e4772c00f
keys4words <keys4words@gmail.com>
2022-06-25 15:32:33 +0300

    start websockets

---
+++ websockets/cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"websockets/internal/handlers"
+
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	return mux
+}
